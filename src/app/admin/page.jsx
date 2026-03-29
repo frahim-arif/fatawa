36462d808a69da24c5003160bccdb7a495999a79
frahim arif <frahim9900@gmail.com>
2026-03-29 19:20:27 +0530

    next fix

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import axios from "axios";
-import { useRouter } from "next/navigation";
+import { useRouter } from "navigation";
 
 export default function AdminPage() {
   const router = useRouter();
