5343a82bd7bb539d13b25e8049fb4403e4146126
frahim arif <frahim9900@gmail.com>
2026-03-31 10:05:12 +0530

    add wordpress redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:year/:month/:slug",
+        destination: "/questions/:slug",
+        permanent: true,
+      },
+      {
+        source: "/category/:slug",
+        destination: "/questions/:slug",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
