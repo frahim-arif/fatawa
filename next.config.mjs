d32be935080125b2f6a20cb2493a58f5a9eb2380
frahim arif <frahim9900@gmail.com>
2026-03-31 10:18:55 +0530

    url change

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
     return [
       {
         source: "/:year/:month/:slug",
-        destination: "/questions/:slug",
-        permanent: true,
-      },
-      {
-        source: "/category/:slug",
         destination: "/questions/:slug",
         permanent: true,
       },
