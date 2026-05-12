8dd7ab1e89cedba25ca5f832f99662887a731fb4
frahim arif <frahim9900@gmail.com>
2026-05-12 21:02:08 +0530

    chabg

--- src/app/books/[id]/page.jsx
+++ src/app/books/[id]/page.jsx
@@ -61,9 +61,9 @@ export default async function BookDetail({
             {book.title}
           </h1>
 
-          <p className="text-sm text-gray-500 mt-1">
+          {/* <p className="text-sm text-gray-500 mt-1">
             ✍️ {book.author || "Unknown"}
-          </p>
+          </p> */}
 
         </div>
 
