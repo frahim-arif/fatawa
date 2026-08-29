dbbad0752bca73a54b37a5fb0b4085c486d5a433
frahim arif <frahim9900@gmail.com>
2026-08-29 22:47:11 +0530

    sdfg

--- src/app/page.js
+++ src/app/page.js
@@ -371,7 +371,7 @@ fetch("https://f-backend-vdi1.onrender.com/api/majameen")
 
       </div>
 
-  <div className="-mt-16">
+  <div className="-mt-12">
   <IslamicSlider />
 </div>
       {/* Search Box */}
