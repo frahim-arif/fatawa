9bcf9b03d76ca15c18817847050996f5583fecf6
frahim arif <frahim9900@gmail.com>
2026-09-12 23:01:19 +0530

    sdfg

--- src/app/page.js
+++ src/app/page.js
@@ -401,7 +401,7 @@ export default function HomePage() {
         w-full
         min-h-screen
         overflow-hidden
-        -mt-8
+        -mt-6
         space-y-10
         px-0
         bg-repeat
