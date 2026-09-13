74080d1e639d65ade0787308ce59ef69f2a642c1
frahim arif <frahim9900@gmail.com>
2026-09-13 15:23:53 +0530

    sdfg

--- src/app/components/IslamicSlider.jsx
+++ src/app/components/IslamicSlider.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 const slides = [
   {
     id: 1,
-    image: '/images/slider03.png',
+    image: '/images/ee.png',
     alt: 'اسلامی معلومات',
   },
   {
